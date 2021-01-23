5379c94546a92eca802d9930f71005e4b761ca71
ykyk1218 <attack_no22001@yahoo.co.jp>
2021-01-23 15:15:29 +0900

    feat:github認証ができるようにした

--- kpt/routes/web.php
+++ kpt/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/redirect', function() {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function() {
+    $user = Socialite::driver('github')->user();
+    $token = $user->token;
+    Log::debug($token);
+});
+
