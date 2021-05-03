df63dbfce64c6a13ed0a6ffadd9d981da6dbbd96
NobuhitoARAI <nobht.arai@gmail.com>
2021-05-03 19:43:51 +0900

    add: webpack settings

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,20 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
+/**
+ * Webpackの設定
+ */
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': __dirname + '/resources/'
+        }
+    },
+})
+
+/**
+ * 各ページで使用するJavaScript
  */
 
 mix.js('resources/js/app.js', 'public/js')
