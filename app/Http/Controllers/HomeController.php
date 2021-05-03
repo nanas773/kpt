21d97d4ac0e8dec779978a23eba609ad51d3082f
NobuhitoARAI <nobht.arai@gmail.com>
2021-05-03 19:41:10 +0900

    add: Add variables to pass to frontend.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,15 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $loginUrl = route('login.github');
+        return view('home.index', [
+                'title' => 'Login',
+                'description' => '',
+                'keywords' => 'KPT',
+                'scriptVars' => [
+                    'loginUrl' => $loginUrl
+                ],
+            ]);
     }
 
     public function callback()
