7829d74ad057526428a6d3f16289250589f43999
NobuhitoARAI <nobht.arai@gmail.com>
2021-05-06 21:18:50 +0900

    add: logout pages component.

---
+++ resources/components/pages/Logout.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script lang="ts">
+export default {
+    name: "Logout"
+}
+</script>
+
+<style scoped>
+
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,3 +19,4 @@ mix.webpackConfig({
  * 各ページで使用するJavaScript
  */
 mix.vue3('resources/js/pages/home/index.js', 'public/js/pages/home/index.js');
+mix.vue3('resources/js/pages/home/logout.ts', 'public/js/pages/home/logout.js');
