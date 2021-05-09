9b05eafd75481b9859d4d49b593695eec1a28a78
NobuhitoARAI <nobht.arai@gmail.com>
2021-05-09 11:58:34 +0900

    add: vue template.

---
+++ resources/components/pages/project/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+<!--FIXME: @M9k21 ここにモック(HTML)を書いてください（修正が終わりましたらこのコメントは削除してください） -->
+    プロジェクト画面の予定
+</template>
+
+<script>
+export default {
+    name: "Detail"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/pages/project/detail.js
@@ -0,0 +1,4 @@
+import {createApp} from "vue"
+import Detail from "@components/pages/project/Detail.vue"
+
+createApp(Detail).mount('#app')
