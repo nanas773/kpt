885bb54c5d9fc501af2eb82f65d091f34b10f5eb
NobuhitoARAI <nobht.arai@gmail.com>
2021-05-09 16:47:28 +0900

    add: team component.

---
+++ resources/components/pages/team/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <!--FIXME: @mb-c16h18n3scl31985 ここにモック(HTML)を書いてください（修正が終わりましたらこのコメントは削除してください） -->
+    チーム画面の予定
+</template>
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
