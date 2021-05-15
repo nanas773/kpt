21eeb3782e76e362e75b0d3fd76abd7f7f485905
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-05-15 11:50:03 +0900

    Add: ヘッダーの追加

--- resources/components/pages/team/Index.vue
+++ resources/components/pages/team/Index.vue
@@ -1,14 +1,22 @@
 <template>
-    <!--FIXME: @mb-c16h18n3scl31985 ここにモック(HTML)を書いてください（修正が終わりましたらこのコメントは削除してください） -->
-    チーム画面の予定
+  <!--FIXME: @mb-c16h18n3scl31985 ここにモック(HTML)を書いてください（修正が終わりましたらこのコメントは削除してください） -->
+
+    <!-- いずれ増えてった時ようにヘッダーもfixedにしたほうがいいのだろうか -->
+    <header class="grid grid-cols-6 bg-gray-100">
+      <h1 class="col-start-2 col-span-4 text-center text-5xl py-10">
+        チーム名
+      </h1>
+      <a href="#" class="col-span-1 text-center my-auto">管理</a>
+      <!-- チーム管理へのリンクはいずれアイコンにする予定 -->
+    </header>
+
 </template>
 
 <script>
 export default {
-    name: "Index"
-}
+  name: "Index",
+};
 </script>
 
 <style scoped>
-
 </style>
