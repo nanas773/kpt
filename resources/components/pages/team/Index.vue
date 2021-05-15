6d1b27c1b2aa558b0998f84850596c90491bf1f6
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-05-15 11:50:51 +0900

    Add: メイン(プロジェクト名)の追加

--- resources/components/pages/team/Index.vue
+++ resources/components/pages/team/Index.vue
@@ -1,6 +1,4 @@
 <template>
-  <!--FIXME: @mb-c16h18n3scl31985 ここにモック(HTML)を書いてください（修正が終わりましたらこのコメントは削除してください） -->
-
     <!-- いずれ増えてった時ようにヘッダーもfixedにしたほうがいいのだろうか -->
     <header class="grid grid-cols-6 bg-gray-100">
       <h1 class="col-start-2 col-span-4 text-center text-5xl py-10">
@@ -9,6 +7,26 @@
       <a href="#" class="col-span-1 text-center my-auto">管理</a>
       <!-- チーム管理へのリンクはいずれアイコンにする予定 -->
     </header>
+
+    <main>
+      <ul class="bg-gray-400 mx-16 my-2 text-xl">
+        <li>
+          <a href="#" class="block p-5 bg-pink-200">プロジェクト名</a>
+        </li>
+        <li class="mt-2">
+          <a href="#" class="block p-5 bg-yellow-200">プロジェクト名</a>
+        </li>
+        <li class="mt-2">
+          <a href="#" class="block p-5 bg-green-200">プロジェクト名</a>
+        </li>
+        <li class="mt-2">
+          <a href="#" class="block p-5 bg-blue-200">プロジェクト名</a>
+        </li>
+        <li class="mt-2">
+          <a href="#" class="block p-5 bg-purple-200">プロジェクト名</a>
+        </li>
+      </ul>
+    </main>
 
 </template>
 
