b7b8d0f9ae76e3c1fdf912fcabd1fb9524d01eff
shimada <mkteaaa@gmail.com>
2021-05-22 22:36:40 +0900

    add: 目標項目の追加

--- resources/components/pages/project/Detail.vue
+++ resources/components/pages/project/Detail.vue
@@ -1,14 +1,31 @@
 <template>
-<!--FIXME: @M9k21 ここにモック(HTML)を書いてください（修正が終わりましたらこのコメントは削除してください） -->
-    プロジェクト画面の予定
+  <!-- 目標の項目 -->
+  <div class="w-4/5 mx-auto my-8 p-3 bg-gray-200">
+    <div class="flex justify-between">
+      <p class="m-2 p-2">2021/04/30</p>
+      <p class="flex-auto bg-white m-2 p-2">目標1</p>
+      <button class="bg-blue-500 text-white rounded m-2 p-2">Completed!</button>
+    </div>
+    <!-- 以下 v-for でまわす -->
+    <div class="flex justify-between">
+      <p class="m-2 p-2">2021/06/30</p>
+      <p class="flex-auto bg-white m-2 p-2">目標2</p>
+      <button class="bg-blue-500 text-white rounded m-2 p-2">Completed!</button>
+    </div>
+    <div class="flex justify-between">
+      <p class="m-2 p-2">2021/08/31</p>
+      <p class="flex-auto bg-white m-2 p-2">目標3</p>
+      <button class="bg-blue-500 text-white rounded m-2 p-2">Completed!</button>
+    </div>
+    <!-- v-for 終了 -->
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Detail"
-}
+  name: "Detail",
+};
 </script>
 
 <style scoped>
-
 </style>
