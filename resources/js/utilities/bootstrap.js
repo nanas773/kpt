70e80d7e46a738cdd2ae83b9e0d47d30f5cea290
NobuhitoARAI <nobht.arai@gmail.com>
2021-06-19 11:23:58 +0900

    csrf-tokenを追加する処理を追加

--- resources/js/utilities/bootstrap.js
+++ resources/js/utilities/bootstrap.js
@@ -8,6 +8,14 @@ window.axios = require('axios')
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
+const csrfToken = document.head.querySelector('meta[name="csrf-token"]');
+if (csrfToken) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken.content;
+} else {
+    console.error('CSRF token not found.');
+}
+
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
