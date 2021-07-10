3d13e5820d26d501c96cbd30a9b110cbba7bca59
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-07-10 10:50:29 +0900

    Add: ユーザー情報表示テーブル作成

--- resources/components/pages/user/Info.vue
+++ resources/components/pages/user/Info.vue
@@ -12,6 +12,22 @@
     </header>
 
     <main>
+      <table class="text-xl w-1/2 table-fixed border border-gray-300 mx-auto mt-28">
+        <tbody>
+        <tr>
+          <th class="p-6 w-1/2 border border-gray-300" scope="row">メールアドレス</th>
+          <td class="p-6 w-1/2 border border-gray-300">nanas@world.com</td>
+        </tr>
+        <tr>
+          <th class="p-6 border border-gray-300" scope="row">名前</th>
+          <td class="p-6 border border-gray-300">ななちゃん</td>
+        </tr>
+        <tr>
+          <th class="p-6 border border-gray-300" scope="row">プロフィール画像</th>
+          <td class="p-6 border border-gray-300"><img src="#" alt="ななちゃんのプロフィール画像"></td>
+        </tr>
+        </tbody>
+      </table>
     </main>
 
   </div>
